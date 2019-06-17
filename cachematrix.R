695e1b297a89d7d06f4cf7dd1b5a71feb479e3bb
Prasiddha Sarma <prasiddha.sarma@gmail.com>
2019-06-17 07:14:14 +0530

    Updated

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,7 @@
 # * cacheInverse   get the cahced value (inverse of the matrix)
 # * getInverse     get the cahced value (inverse of the matrix)
 #
-# Notes:
+#  Special Notes:
 # not sure how the "x = numeric()" part works in the argument list of the 
 # function, but it seems to be creating a variable "x" that is not reachable 
 # from the global environment, but is available in the environment of the 
